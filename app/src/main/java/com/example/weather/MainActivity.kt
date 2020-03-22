b2222e97d8238d502d176547f18b42d024ec7f0d
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-03-22 19:30:25 +0100

    feat: remove fragments

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,16 +1,114 @@
 package com.example.weather
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.dto.RootResponse
+import com.example.weather.model.WeatherEntity
+import kotlinx.android.synthetic.main.card_error.card_error
+import kotlinx.android.synthetic.main.card_error.label_error
+import kotlinx.android.synthetic.main.card_input.input_city
+import kotlinx.android.synthetic.main.card_loading.card_loading
+import kotlinx.android.synthetic.main.card_weather.card_info
+import kotlinx.android.synthetic.main.card_weather.value_city_name
+import kotlinx.android.synthetic.main.card_weather.value_datetime
+import kotlinx.android.synthetic.main.card_weather.value_description
+import kotlinx.android.synthetic.main.card_weather.value_icon
+import kotlinx.android.synthetic.main.card_weather.value_pressure
+import kotlinx.android.synthetic.main.card_weather.value_sunrise
+import kotlinx.android.synthetic.main.card_weather.value_sunset
+import kotlinx.android.synthetic.main.card_weather.value_temperature
+import kotlinx.android.synthetic.main.card_welcome.card_welcome
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.DateFormat
 
 class MainActivity : AppCompatActivity() {
+    private val apiService: WeatherApiService by lazy {
+        WeatherApiService.create()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fragment_container, InputFragment())
-            .commit()
+        showWelcome()
+
+        input_city.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().isEmpty()) {
+                    showWelcome()
+                    return
+                }
+
+                showLoading()
+
+                apiService
+                    .getWeatherData(s.toString())
+                    .enqueue(WeatherDataResponseHandler())
+            }
+        })
+    }
+
+    private inner class WeatherDataResponseHandler : Callback<RootResponse> {
+        override fun onResponse(call: Call<RootResponse>, response: Response<RootResponse>) {
+            if (response.code() != 200) {
+                showError(response.message())
+                return
+            }
+
+            val rootResponse: RootResponse = response.body()!!
+            showData(rootResponse.format())
+        }
+
+        override fun onFailure(call: Call<RootResponse>, t: Throwable) {
+            showError(t.message ?: "Error while loading data")
+        }
+    }
+
+    private fun showWelcome() {
+        card_welcome.visibility = View.VISIBLE
+        card_loading.visibility = View.GONE
+        card_error.visibility = View.GONE
+        card_info.visibility = View.GONE
+    }
+
+    private fun showLoading() {
+        card_welcome.visibility = View.GONE
+        card_loading.visibility = View.VISIBLE
+        card_error.visibility = View.GONE
+        card_info.visibility = View.GONE
+    }
+
+    private fun showError(message: String) {
+        card_welcome.visibility = View.GONE
+        card_loading.visibility = View.GONE
+        card_error.visibility = View.VISIBLE
+        card_info.visibility = View.GONE
+
+        label_error.text = message
+    }
+
+    private fun showData(data: WeatherEntity) {
+        card_welcome.visibility = View.GONE
+        card_loading.visibility = View.GONE
+        card_error.visibility = View.GONE
+        card_info.visibility = View.VISIBLE
+
+        value_city_name.text = data.cityName
+        value_icon.text = data.icon
+        value_description.text = data.description
+        value_temperature.text = data.temperature.toString()
+        value_pressure.text = data.pressure.toString()
+        value_sunrise.text = DateFormat.getTimeInstance().format(data.sunrise)
+        value_sunset.text = DateFormat.getTimeInstance().format(data.sunset)
+        value_datetime.text = DateFormat.getDateTimeInstance().format(data.datetime)
     }
 }
